bd230de1f493dec00cb6bb3a55ac00e55e069efa
Tsadriu <40073056+Tsadriu@users.noreply.github.com>
2022-01-12 12:14:55 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+#import tkinter
+#import os
+
+#root = tkinter.Tk()
+
+#canvas1 = tkinter.Canvas(root, width=300, height=300)
+#canvas1.pack()
+#ROOT_DIR = os.path.dirname(os.path.abspath("top_level_file.txt"))
+#print(ROOT_DIR)
+#file.write("")
+
+#def hello():
+#    label1 = tkinter.Label(root, text='Hello World!', fg='green', font=('helvetica', 12, 'bold'))
+#    canvas1.create_window(150, 200, window=label1)
+
+
+#button1 = tkinter.Button(text='Click Me', command=hello, bg='brown', fg='white')
+#canvas1.create_window(150, 150, window=button1)
+
+#root.mainloop()
+import textAdventure
+textAdventure.start()
---
+++ minigames/textAdventure/textAdventure.py
@@ -0,0 +1,2 @@
+def start():
+    print("This is the start of the adventure!")
